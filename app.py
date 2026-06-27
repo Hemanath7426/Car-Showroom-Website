8a49ca2298df84d3485b0119d1b7005d748b2b42
Hemanath7426 <hemanath388@gmail.com>
2026-06-27 11:14:01 +0530

    Fix formatting of app.run host parameter

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def brand_page(brand_slug):
     return render_template('models.html', brand=brand_data)
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0)
+    app.run(debug=True,host="0.0.0.0")
